06abb91855d4c8868c0fd96b73479e8f2b178b18
Joshua Rymer <joshuarymer1@gmail.com>
2023-02-02 16:08:43 +0000

    Add buildCard and libraryCheck functions

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(book) {
-  if (book.constructor === Book) {
-    myLibrary.push(book);
-  }
+    if (book.constructor === Book) {
+        myLibrary.push(book);
+    }
+}
+
+function buildCard(book) {
+    const container = document.getElementById('card-con');
+    const card = document.createElement('div');
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const pages = document.createElement('div');
+    const read = document.createElement('div');
+
+    card.classList.add('card');
+
+    card.append(title, author, pages, read);
+
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages;
+    read.innerText = book.read;
+
+    container.appendChild(card);
 }
 
 function libraryCheck() {
+    myLibrary.forEach((element) => buildCard(element));
 }
 
 const metamorphoses = new Book('Metamorphoses', 'Ovid', 350, 'not read');
@@ -23,3 +44,5 @@ const aeneid = new Book('The Aeneid', 'Virgil', 250, 'not read');
 addBookToLibrary(metamorphoses);
 
 addBookToLibrary(aeneid);
+
+libraryCheck();
